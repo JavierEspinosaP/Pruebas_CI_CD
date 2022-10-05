82bb61e4aa4326c2282cca97ad2c0708697147af
Javi Espinosa <103537170+JavierEspinosaP@users.noreply.github.com>
2022-10-05 11:04:17 +0200

    fix/eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,9 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    es2020: true
+    es2020: true,
+    jest:true,
+    node:true
   },
   extends: 'eslint:recommended',
   parserOptions: {
